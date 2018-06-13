2c2f120020dae4686db2dd5bc6985654e3fceff8
Lee Martin <lee.b.martin@gmail.com>
2018-06-12 17:30:29 -0700

    prettying

--- promising_results_lab/app.js
+++ promising_results_lab/app.js
@@ -1,29 +1,38 @@
 slowMath.add(6, 2).then(function(value) {
     console.log(value);
-    slowMath.multiply(value, 2).then(function(value) {
-        console.log(value);
-        slowMath.divide(value, 4).then(function(value) {
-            console.log(value);
-            slowMath.subtract(value, 3).then(function(value) {
-                console.log(value);
-                slowMath.add(value, 98).then(function(value) {
-                    console.log(value);
-                    slowMath.remainder(value, 2).then(function(value) {
-                        console.log(value);
-                        slowMath.multiply(value, 50).then(function(value) {
-                            console.log(value);
-                            slowMath.divide(value, 40).then(function(value) {
-                                console.log(value);
-                                slowMath.add(value, 32).then(function(value) {
-                                    console.log(`The final result is ${value}`);
-                                })
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    })
-}).catch(function(error) {
+    return slowMath.multiply(value, 2);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.divide(value, 4);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.subtract(value, 3);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.add(value, 98);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.remainder(value, 2);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.multiply(value, 50);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.remainder(value, 40);
+  })
+  .then(function(value) {
+    console.log(value);
+    return slowMath.add(value, 32);
+  })
+  .then(function(value) {
+    console.log(`The final result is ${value}`);
+  })
+  .catch(function(error) {
     console.log(error);
-});
+  });
